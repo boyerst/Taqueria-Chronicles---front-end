91aec27e6ec836d57eba29f67c18e26a04915ec1
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-02 17:58:27 -0500

    built basic structure of taqueria list

--- src/TaqueriaList/index.js
+++ src/TaqueriaList/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Rating } from 'semantic-ui-react'
 
 export default function TaqueriaList(props) {
   console.log("Here is props in TaqueriaList")
   console.log(props)
+  // const formattedDate = Moment(taquerias.created_at).format("LL")
   const taquerias = props.taquerias.map(taquerias => {
     return(
-      <Card>
+      <Card key={taquerias.id}>
         <Card.Content>
-          <Card.Header>
-            {taquerias.name}
-          </Card.Header>
+          <Card.Header>{taquerias.name}</Card.Header>
+          <Card.Meta>
+            {taquerias.rating}
+          </Card.Meta>
+          <Rating icon="star" type="custom" value={taquerias.rating}/>
+          <Card.Description>
+            What to eat: {taquerias.recommendations}
+          </Card.Description>
+          <Card.Meta textAlign={"left"}>
+            {taquerias.address}
+          </Card.Meta>
+          <Card.Meta textAlign={"left"}>
+            <span 
+            className='date'>
+            Posted on {taquerias.created_at} by {taquerias.patron_id.username}
+            </span>
+          </Card.Meta>
         </Card.Content>
       </Card>
 
